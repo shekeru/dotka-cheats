c8937e8bf82e16af8ffb870f789a2dca86b3c1b7
Sheky Sheks <sheks@desu.systems>
2019-04-01 19:03:14 -0400

    getOwner works

--- Library/EventHooks.cpp
+++ Library/EventHooks.cpp
@@ -11,15 +11,26 @@ bool FireEventClientSide(CGameEventManager *thisptr, CGameEvent *event) {
 			(CDotaPlayer*) client.entities->GetBaseEntity(playerId + 1);
 		cout << "Player Death Event: " << victim << endl;
 		cout << " [+] BindingName: " << victim->SchemaDynamicBinding()->bindingName << endl;
-		Datamap* pred = victim->GetPredDescMap();
-		cout << " [+] PreDescMap: " << pred << endl;
-		cout << "  [-] MapLength: " << pred->numFields << endl;
 		cout << " [+] PlayerName: " << victim->GetPlayerName() << endl;
-		cout << " [+] CharacterName: " << victim->GetCharacterDisplayName() << endl;
-		CDotaBaseNPC* hero = (CDotaBaseNPC*)victim;
-		cout << " [+] CurrentMana: " << hero->GetMana() << endl;
-		cout << " [+] GetMaxMana: " << hero->GetMaxMana() << endl;
-		cout << " [+] BaseNPC VMT: " << hex << *(uintptr_t*)hero << endl;
+		cout << " [+] Is Same Team: " << victim->C_BaseEntity__InLocalTeam() << endl;
+		// try to find
+		for (int EntityIndex = 0; EntityIndex <= client.entities->GetHighestEntityIndex(); EntityIndex++)
+			if (auto entity = client.entities->GetBaseEntity(EntityIndex))
+				if (strstr(entity->SchemaDynamicBinding()->bindingName, "DOTA_Unit_Hero")) {
+					CDotaBaseNPC* hero = (CDotaBaseNPC*) entity;
+					if (hero->GetPlayerOwnerID() != playerId)
+						continue;
+					cout << " [+] BindingName: " << hero->SchemaDynamicBinding()->bindingName << endl;
+					Datamap* pred = hero->GetPredDescMap();
+					cout << " [+] PreDescMap: " << pred << endl;
+					cout << "  [-] MapLength: " << pred->numFields << endl;
+					cout << " [+] CurrentMana: " << hero->GetMana() << endl;
+					cout << " [+] GetMaxMana: " << hero->GetMaxMana() << endl;
+					cout << " [+] PlayerOwner: " << victim << " | " << hero->GetPlayerOwner() 
+						<< " ==> " << hero->GetPlayerOwnerID() << endl;
+					cout << " [+] Damages: " << dec << hero->C_DOTA_BaseNPC__GetDamageMin()
+						<< " - " << hero->C_DOTA_BaseNPC__GetDamageMax() << endl;
+				}		
 		//cout << "\tName: " << victim->GetPlayerName() << endl;
 		//cout << "\tName: " << victim->GetCharacterDisplayName() << endl;
 		cout << endl;
